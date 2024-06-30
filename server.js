e7b559171dd9566310bdd4f9b85783111ffc010e
devance <dj788@snu.edu.in>
2024-06-30 23:16:32 +0530

    cors..

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: '*' }));
+// app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.json());
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/menus', require('./routes/menuRoutes'));
